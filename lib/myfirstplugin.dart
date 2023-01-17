59c2dde76ba26d23fb40451c687634ef5b49e43a
TumuAparna <tumu.aparna@symphonysummit.com>
2023-01-17 18:02:18 +0530

    support for lower versions

--- lib/myfirstplugin.dart
+++ lib/myfirstplugin.dart
@@ -5,13 +5,13 @@ import 'package:flutter/services.dart';
 class Myfirstplugin {
   static const MethodChannel _channel = const MethodChannel('myfirstplugin');
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-  static Future<String?> getCustomData(dynamic data) async {
-    final String? response = await _channel
+  static Future<String> getCustomData(dynamic data) async {
+    final String response = await _channel
         .invokeMethod('getCustomData', <String, dynamic>{'query': data});
     return response;
   }
